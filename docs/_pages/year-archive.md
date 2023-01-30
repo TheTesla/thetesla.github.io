f8bbbbef5840eb359732093a2f6230ee450519dc
Stefan Helmert <stefan.helmert@t-online.de>
2023-01-30 22:28:40 +0100

    year archive

---
+++ docs/_pages/year-archive.md
@@ -0,0 +1,9 @@
+---
+title: Blog-Archiv
+layout: posts
+permalink: /year-archive/
+collection: posts
+entries_layout: posts
+classes: wide
+---
+
--- docs/_site/assets/js/lunr/lunr-store.js
+++ docs/_site/assets/js/lunr/lunr-store.js
@@ -14,6 +14,12 @@ var store = [{
         "title": "Welcome to Jekyll!",
         "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
         "tags": [],
+        "url": "/jekyll/update/2020/11/12/welcome-to-jekyll.html",
+        "teaser": "/assets/images/Profilbild240.webp"
+      },{
+        "title": "Welcome to Jekyll!",
+        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
+        "tags": [],
         "url": "/jekyll/update/2022/11/12/welcome-to-jekyll.html",
         "teaser": "/assets/images/Profilbild240.webp"
       },{
